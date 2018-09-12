829a07a2cec2cc24bd9c3e2c95b9112e711058be
霍卓玺 <azzuro.huo@gmail.com>
2018-09-12 13:29:40 +0800

    updates.

--- update-local-repo.py
+++ update-local-repo.py
@@ -6,20 +6,36 @@ import sys
 from subprocess import check_output
 from os import path
 
-with open('server.sha1sum','r') as f:
-    for line in f:
-        checksum, fits = line.split()
-        if path.exists(fits):
-            checksum_local = check_output(["sha1sum", fits], shell=True).split()[0].lower()
-            if checksum_local == checksum.lower():
-                print "{:<120}: OK".format(fits)
+server_checksum_file = sys.argv[1]
+try:
+    checksum_algorithm = sys.argv[2]
+except:
+    checksum_algorithm = None
+
+with open('local-repo-missing.txt', 'w') as g:
+    with open(server_checksum_file, 'r') as f:
+        for line in f:
+            checksum, fits = line.split()
+            if path.exists(fits):
+                if checksum_algorithm is not None:
+                    checksum_local = check_output(["{} {}".format(checksum_algorithm, fits)], shell=True).split()[0].lower()
+                    if checksum_local == checksum.lower():
+                        print "{:<120}: OK".format(fits)
+                    else:
+                        print "{:<120}: Conflict".format(fits)
+                        g.write('{}  {}\n'.format(checksum, fits))
+                else:
+                    print "{:<120}: OK".format(fits)
+            elif path.exists(fits+'.gz'):
+                if checksum_algorithm is not None:
+                    checksum_local = check_output(["gzip -d -c {}.gz|{} -".format(fits, checksum_algorithm)], shell=True).split()[0].lower()
+                    if checksum_local == checksum.lower():
+                        print "{:<120}: OK".format(fits+'.gz')
+                    else:
+                        print "{:<120}: Conflict".format(fits+'.gz')
+                        g.write('{}  {}\n'.format(checksum, fits))
+                else:
+                    print "{:<120}: OK".format(fits+'.gz')
             else:
-                print "{:<120}: Conflict".format(fits)
-        elif path.exists(fits+'.gz'):
-            checksum_local = check_output(["gzip -d -c {}.gz|sha1sum -".format(fits)], shell=True).split()[0].lower()
-            if checksum_local == checksum.lower():
-                print "{:<120}: OK".format(fits+'.gz')
-            else:
-                print "{:<120}: Conflict".format(fits+'.gz')
-        else:
-            print "{<:120}: Miss".format(fits)
+                print "{:<120}: Miss".format(fits)
+                g.write('{}  {}\n'.format(checksum, fits))
